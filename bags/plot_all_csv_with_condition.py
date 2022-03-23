7fbd20d5e78248c9a6d75d98e21d7cd1e4bce398
Sakib Ahmed Sumdany <ahmadSum1@users.noreply.github.com>
2022-03-23 23:07:28 +0100

    filter plots

--- bags/plot_all_csv_with_condition.py
+++ bags/plot_all_csv_with_condition.py
@@ -17,13 +17,13 @@ idx = 0
 ### Loop over all files
 for file in fileNames:
 
-    if file.find("0.005_no_wall_2") == -1:
+    if file.find("_g_") != -1:
         continue
 
     ### Read .csv file and append to list
     df = pd.read_csv(PATH + file, index_col = None)
     # bias = np.average (np.array(df['field.data'].tolist())[:5])
-    bias = -0.1
+    bias = -0.09
     # print (bias)
     Y = np.array(df['field.data'].tolist()) - bias
     X = (np.arange(len(Y)) + idx + 10)
@@ -35,12 +35,14 @@ for file in fileNames:
     # break
     
     ### Create line for every file
-    plt.plot( X, Y, label=file.replace("depth_control_12v_", "").replace(".csv", "").replace("_no_wall", "").replace("kp_p", "kp"))
+    plt.plot( X, Y, label=file.replace("depth_control_12v_", "").replace(".csv", "").replace("_no_wall", "").replace("kp_p", "kp")+f', g={0}'+f', Ki={0.001}')
     # plt.plot( X, Y, label=file.replace("depth_control_12v_", "").replace(".csv", "")+"-bias-"+str(bias)[:5])
     plt.rc('legend',fontsize='medium')
     # break
 
 ### Generate the plot
+plt.ylabel("Depth(m)")
+plt.xlabel("Time Steps")
 plt.legend(loc="best")
 # plt.xlim(0, 37250)
 # plt.ylim(-0.68, 0.5)
