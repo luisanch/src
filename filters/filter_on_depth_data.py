423cc85b3615a32be4fcb43196791126637f4a32
Sakib Ahmed Sumdany <ahmadSum1@users.noreply.github.com>
2022-03-23 14:29:39 +0100

    filter comparisn

--- filters/filter_on_depth_data.py
+++ filters/filter_on_depth_data.py
@@ -30,7 +30,6 @@ a = 0.45
 b = 0.1
 g = 0.0000
 
-iteration = 0
 Y_f = np.zeros_like(Y)
 
 for i in range(len(Y)):
@@ -52,21 +51,47 @@ for i in range(len(Y)):
     ak_1 = ak
 
     Y_f[i] = xk
-    # print("xk = " ,xk, "vk = " ,vk, "ak = ",ak)
-    # iteration += 1
-    #plt.axis([0, 20+iteration, 0, 1.5])
-    # plt.plot(iteration,xm, linestyle=".", label='raw data')
-    # plt.plot(iteration,xk, '-o') #,label='filtered data')
-    # plt.pause(0.025)
-    # plt.draw()
 
-plt.plot( X, Y, linewidth=0.75, color='red', linestyle=":", label="raw data")
-plt.plot( X, Y_f, linewidth=0.75, color='green', linestyle="-.", label="filtered data")
+## filter parameters
+dt = 0.02 #50Hz data_read-control loop
+xk_1 = 0
+vk_1 = 0
+ak_1 = 0
+
+a = 0.25
+b = 0.025
+g = 0.0001
+
+Y_f2 = np.zeros_like(Y)
+
+for i in range(len(Y)):
+
+    xm = Y[i]
+
+    xk = xk_1 + (vk_1 * dt)
+    vk = vk_1 + (ak_1 * dt)
+    ak = (vk - vk_1) /dt
+
+    rk = xm - xk
+
+    xk += a*rk
+    vk += (b*rk)/dt
+    ak += (2*g*rk)/np.power(dt,2)
+
+    xk_1 = xk
+    vk_1 = vk
+    ak_1 = ak
+
+    Y_f2[i] = xk
+
+plt.plot( X, Y, linewidth=1.5, color='red', linestyle=":", label="raw data")
+plt.plot( X, Y_f, linewidth=0.75, color='green', linestyle="-.", label=f"filtered data(a=0.45,b=0.1)")
+plt.plot( X, Y_f2, linewidth=0.75, color='blue', linestyle="--", label=f"filtered data(a={a},b={b},g={g})")
 
 ### Generate the plot
-plt.xlabel("Depth(m)")
-plt.ylabel("Time Steps")
+plt.ylabel("Depth(m)")
+plt.xlabel("Time Steps")
 plt.legend(loc="best")
-plt.axhline(-0.5,color='grey',linestyle="--", label='Target depth') # y = 0
+plt.axhline(-0.5,color='grey', label='Target depth') # y = 0
 plt.grid()
 plt.show()
