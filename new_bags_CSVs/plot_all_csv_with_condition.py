e4692598665f68abf5800abad09ac0a9968c5e86
Sakib Ahmed Sumdany <ahmadSum1@users.noreply.github.com>
2022-03-27 08:51:23 +0200

    more plotting

--- new_bags_CSVs/plot_all_csv_with_condition.py
+++ new_bags_CSVs/plot_all_csv_with_condition.py
@@ -1,3 +1,4 @@
+from cProfile import label
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -19,7 +20,7 @@ for file in fileNames:
 
     # if file.find("_g_") != -1:  #skip files with this sub string
     #     continue
-    if file.find(".bag _De") == -1:    #skip files without this sub string
+    if file.find(".bag _Depth") == -1:    #skip files without this sub string
         continue
 
     ### Read .csv file and append to list
@@ -31,13 +32,13 @@ for file in fileNames:
     
 
     
-    df_d['%time'] = (df_d['%time'] - start_time) / 1e9   #nano second to ms
-    df_pwm['%time'] = (df_pwm['%time'] - start_time) / 1e9 #nano second to ms
+    df_d['%time'] = (df_d['%time'] - start_time) / 1e9   #nano second to s
+    df_pwm['%time'] = (df_pwm['%time'] - start_time) / 1e9  #nano second to s
     
     # print(df_d['%time'].iloc[-1])
     # print(df_pwm['%time'].iloc[-1])
     # bias = np.average (np.array(df['field.data'].tolist())[:5])
-    # bias = 0
+    bias = 0.22 
     # print (bias)
     # Y_d = np.array(df_d['field.data'].tolist())
     # Y_pwm = np.array(df_pwm['field.channels2'].tolist())
@@ -56,30 +57,37 @@ for file in fileNames:
     
 
     fig, ax1 = plt.subplots()
+    rho = 1000.0  # 1025.0 for sea water
+    g = 9.80665
 
     color = 'tab:red'
     ax1.set_xlabel('Time (s)')
     ax1.set_ylabel('Depth (m)', color=color)
-    ax1.axhline(0.5,color='grey',linestyle=":") # y = 0
-    line1,  = ax1.plot(df_d['%time'],df_d['field.data'], color=color, label=file.replace(".csv", "").replace("_Depth", "").replace("_step=0.02_pid_xk.bag ", "").replace("_trajectory", ""))
+    ax1.axhline(0.0,color='grey') # y = 0
+    ax1.plot(df_d['%time'],(df_d['field.data']- 101300)/(rho*g)+bias, color=color) #, label=file.replace(".csv", "").replace("_Depth", "").replace("_step=0.02_pid_xk.bag ", "").replace("_trajectory", ""))
     ax1.tick_params(axis='y', labelcolor=color)
+    
+    # ax1.axhline(0.2758,color='green',linestyle="--", label = "ROV Neutrally buoyant") # y = 0
+
     # plt.rc('legend',fontsize='small')
+
 
     # ax1.annotate('Pushed down', xy=(28.5, .46), xytext=(28, .3),
     #         arrowprops=dict(facecolor='black', shrink=0.025),
     #         )
-    # ax1.annotate('pulled up', xy=(38, .3), xytext=(3, .33),
-    #         arrowprops=dict(facecolor='black', shrink=0.05),
-    #         )
+    ax1.annotate('ROV ~Neutrally buoyant', xy=(200, 0.03), xytext=(180, 0.1),
+            arrowprops=dict(facecolor='black', shrink=0.05),
+            )
 
     ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
 
     color = 'tab:blue'
     ax2.set_ylabel('PPM', color=color)  # we already handled the x-label with ax1
-    ax2.plot(df_pwm['%time'],df_pwm['field.channels2'], color=color)
+    ax2.plot(df_pwm['%time'],df_pwm['field.channels2'],  drawstyle='steps-post', color=color)
+    # ax2.plot(df_pwm['%time'],df_pwm['field.channels2'], "-o", color=color)
     ax2.tick_params(axis='y', labelcolor=color)
 
-    fig.suptitle('PID with alpha-beta filtering')
+    # fig.suptitle('With 1.5-Liter empty plastic bottle on the top of the ROV')
     # fig.legend(loc='lower right')
     fig.tight_layout()  # otherwise the right y-label is slightly clipped
 
@@ -87,12 +95,12 @@ for file in fileNames:
     # plt.ylabel("Depth(m)")
     # plt.xlabel("Time Steps")
 
-    # plt.xlim(0, 37250)
+    plt.xlim(150, 280)
     # plt.ylim(-0.68, 0.5)
     
     plt.grid()
     break
 
 
-# fig.legend(loc="best")
+ax1.legend(loc="lower right")
 plt.show()
